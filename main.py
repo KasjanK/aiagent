47210414a9fa3213f2c4ebb37e6a4458159fc2e1
KasjanK <kowalczykkasjan@gmail.com>
2025-07-16 13:42:03 +0200

    refactor: created generate_content so the agent can call multiple functions after eachother

--- main.py
+++ main.py
@@ -4,32 +4,35 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import schema_run_python_file, schema_get_file_content, schema_get_files_info, schema_write_file
-from functions.call_function import call_function
+from call_function import call_function, available_functions
 
 def main():
-    if len(sys.argv) < 2:
-        print("No arguments provided")
-        exit(1)
-
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
 
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args:
+        ("no args provided")
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    user_prompt = sys.argv[1]
+    user_prompt = " ".join(args)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    available_functions = types.Tool(
-        function_declarations=[
-            schema_get_files_info,
-            schema_get_file_content,
-            schema_write_file,
-            schema_run_python_file
-        ]
-    )
+    generate_content(client, messages, verbose)
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
@@ -39,24 +42,26 @@ def main():
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
 
-    verbose = "--verbose" in sys.argv
-
     if verbose:
-        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {prompt_tokens}")
         print(f"Response tokens: {response_tokens}")
 
     if not response.function_calls:
         return response.text
     
+    function_responses = []
     for function_call_part in response.function_calls:
-        function_call_result = call_function(function_call_part, verbose=verbose)
+        function_call_result = call_function(function_call_part, verbose)
 
-        if not function_call_result.parts[0].function_response.response:
-            sys.exit("fatal error")
+        if not function_call_result.parts or not function_call_result.parts[0].function_response:
+            raise Exception("empty function call result")
         
         if verbose:
             print(f"-> {function_call_result.parts[0].function_response.response}")
-            
+            function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+    
 if __name__ == "__main__":
     main()
